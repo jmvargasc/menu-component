17aef9ab15003a08dbaf927f5afd4a10e7fd6cd6
AlanKevinGZ <hades120@hotmail.com>
2024-06-13 15:32:50 -0600

    Cambios al menu

--- menu-component.js
+++ menu-component.js
@@ -1,61 +1,112 @@
 import { LitElement, html, css } from 'lit';
 
 export class MenuComponent extends LitElement {
+
   static properties = {
-    version: {},
     menuItems: { type: Array, attribute: 'menu-items' }
   };
 
   static styles = css`
-    nav {
-      display: flex;
-      justify-content: center;
+    .hamburger-menu{
+      padding:2rem;
     }
-
-    nav ul {
-      list-style-type: none;
-      margin: 0;
-      padding: 0;
-    }
-
-    nav ul li {
-      display: inline;
-      margin-right: 20px;
+    .hamburger-icon {
+      width: 30px;
+      height: 25px;
+      position: relative;
       cursor: pointer;
-      transition: color 0.3s ease; 
     }
 
-    nav ul li:hover {
-      color: #ff0000; 
+    .hamburger-icon span {
+      display: block;
+      position: absolute;
+      height: 4px;
+      width: 100%;
+      background-color: #333;
+      border-radius: 2px;
+      opacity: 1;
+      left: 0;
+      transform: rotate(0deg);
+      transition: 0.25s ease-in-out;
     }
 
-    nav ul li a {
-      color: #fff;
-      text-decoration: none;
+    .hamburger-icon span:nth-child(1) {
+      top: 0px;
+    }
+
+    .hamburger-icon span:nth-child(2) {
+      top: 10px;
+    }
+
+    .hamburger-icon span:nth-child(3) {
+      top: 20px;
+    }
+
+    .hamburger-menu ul {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+      opacity: 0;
+      visibility: hidden;
+      transform: translateY(-20px);
+      transition: opacity 0.25s ease-in-out, visibility 0.25s ease-in-out, transform 0.25s ease-in-out;
+      display:flex;
+      justify-content: space-around;
+      background-color:#333;
+    }
+
+    .hamburger-menu.open ul {
+      opacity: 1;
+      visibility: visible;
+      transform: translateY(0);
+      display:flex;
+      justify-content: space-around;
+      padding:1rem;
+    }
+
+    .hamburger-menu ul li {
+      margin: 10px 0;
+      font-size: 18px;
+      font-weight: bold;
+      color: #ffffff;
+      cursor: pointer;
+      transition: color 0.3s ease;
+    }
+
+    .hamburger-menu ul li:hover {
+      color: #ff6347;
     }
   `;
 
-
   constructor() {
     super();
-    this.version = 'STARTING';
+    this.menuItems = [];
+    this.isOpen = false;
   }
 
-  handleClick(event, item) {
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+    this.requestUpdate();
+  }
+
+  handleItemClick(item) {
     console.log(`Elemento del menú seleccionado: ${item}`);
   }
 
   render() {
     return html`
-      <nav>
+      <div class="hamburger-menu ${this.isOpen ? 'open' : ''}">
+        <div class="hamburger-icon" @click=${this.toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
         <ul>
-          ${this.menuItems.map((item, index) => html`
-            <li key=${index} @click=${(e) => this.handleClick(e, item)}>
-              ${item}
-            </li>
+          ${this.menuItems.map((item) => html`
+            <li @click=${() => this.handleItemClick(item)}>${item}</li>
           `)}
         </ul>
-      </nav>
+      </div>
     `;
   }
 }
